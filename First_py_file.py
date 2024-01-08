0230935a91163a0786c3f408b98ab35d74faf1d0
Vivek Bagul <vivekbagul4@gmail.com>
2024-01-08 23:19:26 +0530

    added multiplication functionality.

--- First_py_file.py
+++ First_py_file.py
@@ -1,2 +1,10 @@
 # this code prints "Hello World" on console
 print("Hellow World")
+
+def multi(*args):
+    ans=1
+    for i in args:
+        ans=ans*i
+    print(ans)
+    return ans
+result=addition(10,20,30)
