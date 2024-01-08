3c973babe06be7031744765988e1888473937723
Vivek Bagul <vivekbagul4@gmail.com>
2024-01-08 23:13:23 +0530

    added one functionality

--- First_py_file.py
+++ First_py_file.py
@@ -1,2 +1,10 @@
 # this code prints "Hello World" on console
 print("Hellow World")
+def addition(*args):
+    ans=0
+    for i in args:
+        ans=ans+i
+    print(ans)
+    return ans
+result=addition(10,20,30)
+print("func completed")
